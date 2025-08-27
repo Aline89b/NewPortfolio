e5845eef99c37ea010e60705ea79f76a13e016b7
aline grianti <a.grianti89@gmail.com>
2025-08-27 19:34:45 +0200

    fixing i18next issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import i18nextIntegration from 'astro-i18next';
 import vercel from "@astrojs/vercel/static"; // usa `static` se vuoi SSG
 
 export default defineConfig({
@@ -10,10 +9,6 @@ export default defineConfig({
   integrations: [
     tailwind(),
     react(),
-    i18nextIntegration({
-      defaultLocale: 'it',
-      locales: ['it', 'en'],
-      fallbackLng: 'it',
-    }),
+    
   ],
 });
