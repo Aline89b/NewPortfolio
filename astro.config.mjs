438b350ce687798fba57625fe26ccbc60e30ff2a
aline grianti <a.grianti89@gmail.com>
2025-09-26 18:05:31 +0200

    deletes adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/static"; // usa `static` se vuoi SSG
+//import vercel from "@astrojs/vercel/static"; // usa `static` se vuoi SSG
 
 export default defineConfig({
   output: "static",
-  adapter: vercel(),
+ // adapter: vercel(),
   integrations: [
     tailwind(),
     react(),
